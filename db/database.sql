426afb8f5deff1c7155ab466efc36af5d9982423
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-05-09 16:55:03 +0200

    Problemen oplossen

--- db/database.sql
+++ db/database.sql
@@ -6,9 +6,10 @@ grant all privileges on database pxldb to secadv;
 BEGIN;
 
 create table users (id serial primary key, user_name text not null unique, password text not null);
+create extension pgcrypto;
 grant all privileges on table users to secadv;
 
-insert into users (user_name, password) values ('pxl-admin', 'insecureandlovinit') ;
-insert into users (user_name, password) values ('george', 'iwishihadbetteradmins') ;
+insert into users (user_name, password)  values ('pxl-admin', crypt('insecureandlovinit', gen_salt('bf'))) ;
+insert into users (user_name, password) values ('george', crypt('iwishihadbetteradmins', gen_salt('bf'))) ;
 
 COMMIT;
